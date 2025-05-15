b8e1aa4fdba638845ac7b197ba96e7d59abc95de
Catalina Hollmann <catahollmann701@gmail.com>
2025-05-14 22:10:49 -0300

    Correccion 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 import ListaCancionesFavoritas from './paginas/ListaCancionesFavoritas';
 import DetalleAlbum from './paginas/DetalleAlbum';
 import Inicio from './paginas/Inicio.jsx';
@@ -6,13 +8,27 @@ import Login from './paginas/Login.jsx';
 import DetalleArtista from './paginas/DetalleArtista';
 
 export default function App() {
+ 
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    setToken(storedToken);
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<Inicio />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/artista/:id" element={<DetalleArtista />} />
-      <Route path="/album/:id" element={<DetalleAlbum />} /> 
-      <Route path="/favoritos" element={<ListaCancionesFavoritas />} />
+      <Route path="/login" element={<Login setToken={setToken} />} />
+      {token ? (
+        <>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/artista/:id" element={<DetalleArtista />} />
+          <Route path="/album/:id" element={<DetalleAlbum />} />
+          <Route path="/favoritos" element={<ListaCancionesFavoritas />} />
+        </>
+      ) : (
+        <Route path="" element={<Navigate to="/login" replace />} />
+      )}
     </Routes>
   );
 }
